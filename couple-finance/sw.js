cf3c6a39bcf88f954eb2423ff8bb028475a3268c
hyunjolee-cell <hyunjo.lee@beaverworksinc.com>
2026-07-19 23:21:00 +0900

    Clear stale finance app cache

--- couple-finance/sw.js
+++ couple-finance/sw.js
@@ -1,11 +1,15 @@
-const CACHE='couple-finance-v2';
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./']))));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+const CACHE='couple-finance-v3';
+self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
+self.addEventListener('activate',event=>event.waitUntil((async()=>{
+  const keys=await caches.keys();
+  await Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)));
+  await self.clients.claim();
+})()));
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
-  event.respondWith(fetch(event.request).then(response=>{
+  event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
     const copy=response.clone();
-    caches.open(CACHE).then(c=>c.put(event.request,copy));
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
     return response;
   }).catch(()=>caches.match(event.request)));
 });
